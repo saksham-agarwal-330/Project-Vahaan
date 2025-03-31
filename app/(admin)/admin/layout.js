670a92cf383cd451406eeca9563db8dd25dd41af
Saksham Agarwal <sakshamagarwal330@gmail.com>
2025-03-31 23:58:19 +0530

    fix package.json

--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -4,19 +4,26 @@ import { notFound } from 'next/navigation'
 import React from 'react'
 import Sidebar from './_components/sidebar'
 
-const AdminLayout = async({children}) => {
-    const admin =await getAdmin()
-    if (!admin.authorized) {
-        return notFound()
-    }
+const AdminLayout = async ({ children }) => {
+  const admin = await getAdmin()
+  if (!admin.authorized) {
+    return notFound()
+  }
   return (
-    <div className='h-full'>
-        <Header isAdminPage={true} />
-        <div className='flex h-full w-56 flex-col top-20 fixed inset-y-0 z-50'>
+    <html>
+      <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </head>
+      <body>
+        <div className='h-full'>
+          <Header isAdminPage={true} />
+          <div className='flex h-full w-56 flex-col top-20 fixed inset-y-0 z-50'>
             <Sidebar />
+          </div>
+          <main className='md:pl-56 pt-[80px] h-full'>{children}</main>
         </div>
-        <main className='md:pl-56 pt-[80px] h-full'>{children}</main>
-    </div>
+      </body>
+    </html>
   )
 }
 
